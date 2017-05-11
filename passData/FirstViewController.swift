590ccfc2cc717fac97132d364305a683526132c6
sharon mcdermott <sharonmcdermott@me.com>
2017-05-11 05:13:53 -0400

    Created a second view controller and connected it to the storyboard.  Renamed the view controller to FirstViewController and connected it to the stpryboard.

--- passData/FirstViewController.swift
+++ passData/FirstViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  FirstViewController.swift
 //  passData
 //
 //  Created by sharon mcdermott on 5/11/17.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FirstViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
